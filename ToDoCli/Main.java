1e6267f532d3cfd8308675f12495c9987d1d272d
Maciej Lisowski <macieejl00@gmail.com>
2021-02-11 18:58:33 +0100

    Started validating user input

--- ToDoCli/Main.java
+++ ToDoCli/Main.java
@@ -3,10 +3,13 @@ import java.util.HashSet;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main{
   private Set<ToDo> td;
   private String[] menuOptions, tskCreate;
+  private BufferedReader br;
   {
     td = new HashSet<ToDo>();
     menuOptions = new String[]{"[1] Add ToDo",
@@ -14,36 +17,41 @@ public class Main{
                                 "[3] Finish ToDo",
                                 "[4] Add Task to ToDo",
                                 "[5] Finish Task"};
-    tdCreate = new String[]{"Name: ",
-                            "PredictedDate: ",
-                            "Importance: "};
+    tskCreate = new String[]{"Name: ",
+                            "Predicted Date YYYY-MM-DD: ",
+                            "Importance:%n\t[1]Low%n\t[2]Medium%n[3]\tHigh "};
+    br = new BufferedReader(new InputStreamReader(System.in));
   }
   public static void main(String... args){
     Main m = new Main();
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    m.createLoop(br);
+    m.createLoop();
   }
-  private void createLoop(BufferedReader br){
+  private void createLoop(){
     String userChoiceS = "";
     int userChoice = 0;
     printMenu();
-    try{
-      while(!(userChoiceS = br.readLine()).isEmpty()){
-        userChoice = Integer.valueOf(userChoiceS);
-        chooseOption(userChoice);
-        printMenu();
-      }
-    } catch(IOException e){}
+    while(!(userChoiceS = getInput()).isEmpty()){
+      userChoice = parseUserInt(userChoiceS);
+      if(userChoice!=0) chooseOption(userChoice);
+      printMenu();
+    }
   }
   private void createTodo(){
-    //TODO: Create body
-    System.out.println("Test1");
+    String ui = "";
+    Boolean isValid = false;
+    for(String s : tskCreate){
+      System.out.printf(s);
+      do{
+        ui = getInput();
+        isValid = validateInput(ui);
+      }while(!isValid);
+    }
   }
   private void finishTodo(){
     //TODO: Create body
     System.out.println("Test2");
   }
-  private void chooseTodo(){
+  private void chooseTodo(int c){
     //TODO: Create body
     System.out.println("Test3");
   }
@@ -62,10 +70,8 @@ public class Main{
         printToDos();
         break;
       case 3:
-        chooseTodo();
-        break;
       case 4:
-        chooseTodo();
+        chooseTodo(choice);
         break;
       case 5:
         finishTask();
@@ -74,7 +80,7 @@ public class Main{
   }
   private void printMenu(){
     System.out.println("What do you want to do?");
-    for(String s : menuOptions) System.out.println(s);
+    for(String s : menuOptions) System.out.println("\t"+s);
   }
   private void printToDos(){
     for(ToDo todo : td){
@@ -83,5 +89,27 @@ public class Main{
         System.out.println(task.toString());
       }
     }
+  }
+  private Integer parseUserInt(String s){
+    try{
+      return Integer.parseInt(s);
+    } catch(NumberFormatException e){
+      System.out.println("Please enter a digit specified in []");
+      return 0;
+    }
+  }
+  private String getInput(){
+    try{
+      return br.readLine();
+    }catch(IOException e){
+      System.out.println("Sorry, there must have been an error");
+      return "";
+    }
+  }
+  private Boolean validateInput(String s){
+    if(s.isEmpty()) return false;
+    Pattern p = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
+    Matcher m = p.matcher(s);
+    return m.matches();
   }
 }
